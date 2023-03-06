31986f7657bc33904c43f213aaa0f2013b48fa60
Andrzej Szulc <andrzej.szulc@gmail.com>
2023-03-06 17:40:06 +0100

    Fix starting script

--- start_db_app.sh
+++ start_db_app.sh
@@ -2,8 +2,9 @@
 
 source .env
 
+sudo apt-get update
+
 # below is needed if we want to host mysql locally
-#sudo apt-get update
 #sudo apt-get -y install mysql-server
 #sudo mysql --defaults-file=/etc/mysql/debian.cnf -e "CREATE DATABASE ${DB_NAME};USE ${DB_NAME};CREATE TABLE Users (UserID int, LastName varchar(255), FirstName varchar(255));INSERT INTO Users (UserID, LastName, FirstName) VALUES (1, 'Placek', 'Jacek'),(2, 'Niejadek', 'Tadek'),(3, 'Awaria', 'Maria');"
 #sudo mysql --defaults-file=/etc/mysql/debian.cnf -e "CREATE USER '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASSWORD}';CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';GRANT ALL ON *.* TO '${DB_USER}'@'localhost';GRANT ALL ON *.* TO '${DB_USER}'@'%';FLUSH PRIVILEGES;"
