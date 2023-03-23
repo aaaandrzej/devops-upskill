4f19023a6b926a705e04f6116ecb8ab91658bc89
Andrzej Szulc <andrzej.szulc@gmail.com>
2023-03-23 16:50:35 +0100

    Remove obsolete s3 app instances from tf

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -34,25 +34,3 @@ resource "aws_instance" "db_app" {
     Owner = var.owner
   }
 }
-
-resource "aws_instance" "s3_app" {
-  count                = local.az_count
-  ami                  = data.aws_ami.ubuntu.id
-  instance_type        = var.instance_size
-  subnet_id            = aws_subnet.private_subnets[count.index].id
-  security_groups      = [aws_security_group.s3_app.id]
-  key_name             = aws_key_pair.kp.id
-  iam_instance_profile = aws_iam_instance_profile.ec2_instance_profile.id
-  user_data = templatefile(
-    "${path.module}/userdata/s3_app_userdata.sh.tftpl",
-    {
-      db_app_host    = aws_lb.db_apps.dns_name
-      s3_bucket_name = aws_s3_bucket.main.id
-    }
-  )
-
-  tags = {
-    Name  = join("-", [var.owner, "s3-app", ["a", "b"][count.index]])
-    Owner = var.owner
-  }
-}
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,16 +3,6 @@ output "bastion_ip_address" {
   description = "The IP address of the bastion"
 }
 
-output "db_apps_ip_addresses" {
-  value       = aws_instance.db_app[*].private_ip
-  description = "The IP addresses of the db apps"
-}
-
-output "s3_apps_ip_addresses" {
-  value       = aws_instance.s3_app[*].private_ip
-  description = "The IP addresses of the s3 apps"
-}
-
 output "bucket-name" {
   value       = aws_s3_bucket.main.id
   description = "The name of the bucket"
