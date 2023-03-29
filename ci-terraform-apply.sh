bc5a33db51fb445f5e05e55f2638f0b1a870ef30
Andrzej Szulc <andrzej.szulc@gmail.com>
2023-03-29 18:57:50 +0200

    Fix tf ci scripts

--- ci-terraform-apply.sh
+++ ci-terraform-apply.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-terraform --chdir=terraform init --reconfigure --input=false
-terraform --chdir=terraform plan --input=false
-terraform --chdir=terraform apply --auto-approve --input=false
+terraform -chdir=terraform init --reconfigure --input=false
+terraform -chdir=terraform plan --input=false
+terraform -chdir=terraform apply --auto-approve --input=false
--- ci-terraform-destroy.sh
+++ ci-terraform-destroy.sh
@@ -2,6 +2,6 @@
 
 set -ex
 
-terraform --chdir=terraform init --reconfigure --input=false
-terraform --chdir=terraform plan --destroy --input=false
-terraform --chdir=terraform destroy --auto-approve --input=false
+terraform -chdir=terraform init --reconfigure --input=false
+terraform -chdir=terraform plan --destroy --input=false
+terraform -chdir=terraform destroy --auto-approve --input=false
