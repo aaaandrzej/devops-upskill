49ac64d6cf26533064c9939e458414b3ffaa0d1a
Andrzej Szulc <andrzej.szulc@gmail.com>
2023-04-25 21:47:39 +0200

    Fix s3 acl issue

--- terraform/storage/main.tf
+++ terraform/storage/main.tf
@@ -11,16 +11,9 @@ resource "aws_s3_bucket" "main" {
   }
 }
 
-resource "aws_s3_bucket_acl" "main" {
+resource "aws_s3_bucket_ownership_controls" "main" {
   bucket = aws_s3_bucket.main.id
-  acl    = "private"
-}
-
-resource "aws_s3_bucket_public_access_block" "main" {
-  bucket = aws_s3_bucket.main.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
 }
